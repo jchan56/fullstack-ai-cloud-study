5dd14b2b6593294177f338b7cb20509022b73af5
jchan56 <166288426+jchan56@users.noreply.github.com>
2024-07-05 14:29:14 +0900

    Delete Week 1 directory

--- Week 1/objectLoop.js
+++
@@ -1,8 +0,0 @@
-let object = {
-    name: '바나나',
-    price: 1200
-};
-
-for (let key in object) {
-    console.log(`${key}: ${object[key]}`);
-}
--- Week 1/test.js
+++
@@ -1,6 +0,0 @@
-const greet = function(name) {
-    console.log("안녕하세요" + name)
-
-};
-
-console.log(greet("진형"));
