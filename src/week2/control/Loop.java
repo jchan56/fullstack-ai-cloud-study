d2fa1263e09dcc71dc429401e54a6bc86c8bc5c6
James Jang <jchan3690@gmail.com>
2024-07-08 10:03:59 +0900

    [Week2]: 반복문 문제_10개의 홀수 랜덤으로 출력

--- src/week2/control/Loop.java
+++ src/week2/control/Loop.java
@@ -1,18 +1,31 @@
 package week2.control;
 
+import java.util.*;
+
 public class Loop {
+    private static final Random rd = new Random();
+
     public static void main(String[] args) {
         forTest();
+        forTest2();
     }
 
     private static void forTest() {
-        for (int i = 10, direction = -1; i > 0; i += direction) {
+        int direction = -1;
+        for (int i = 10; i != 11; i += direction) {
             System.out.print(i + " ");
             if (i == 1) {
                 direction = 1;
             }
         }
-        System.out.print(10);
+        System.out.println(); // 개행 추가
+    }
+
+    private static void forTest2() {
+        for (int i = 0; i < 10; i++) {
+            int number = rd.nextInt(50) * 2 + 1;
+            System.out.print(number + " ");
+        }
+        System.out.println(); // 개행 추가
     }
 }
-
