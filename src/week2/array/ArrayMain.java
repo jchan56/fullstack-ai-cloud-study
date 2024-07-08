c1b81ec79e7b3f41a49af5c3678b8bec14629e5a
James Jang <jchan3690@gmail.com>
2024-07-08 16:41:40 +0900

    [Week2]: 배열 문제_메소드를 활용하여 이름을 입력 받아 환영해주는 프로그램

--- src/week2/array/ArrayMain.java
+++ src/week2/array/ArrayMain.java
@@ -1,11 +1,22 @@
 package week2.array;
 
+import java.util.Scanner;
+
 public class ArrayMain {
     public static void main(String[] args) {
-        arrayTest();
+        Scanner sc = new Scanner(System.in); // 사용자 입력을 받기 위한 Scanner 객체 생성
+
+        System.out.print("회원의 이름을 입력하세요: ");
+        String name = sc.nextLine();
+
+        String greeting = greet(name);
+
+        System.out.println(greeting);
+
+        sc.close();
     }
 
-    private static void arrayTest() {
-        int[] nums;
+    public static String greet(String name) {
+        return "Hello " + name;
     }
 }
