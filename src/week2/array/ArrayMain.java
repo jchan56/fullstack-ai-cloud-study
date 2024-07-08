c6de8bb5b545855ed78184cda50a7453a3643cc2
James Jang <jchan3690@gmail.com>
2024-07-08 11:22:00 +0900

    [Week2]: 배열 예제

---
+++ src/week2/array/ArrayMain.java
@@ -0,0 +1,11 @@
+package week2.array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        arrayTest();
+    }
+
+    private static void arrayTest() {
+        int[] nums;
+    }
+}
